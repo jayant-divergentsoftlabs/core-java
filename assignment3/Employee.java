8c8c76cfca56806347b3f13534be350e3c778b7f
jayant-divergentsoftlabs <80309785+jayant-divergentsoftlabs@users.noreply.github.com>
2021-03-30 14:35:14 +0530

    Update Employee.java

--- assignment3/Employee.java
+++ assignment3/Employee.java
@@ -1,42 +1,35 @@
 package com.divergent.corejava.assignment3;
 
+import java.time.LocalDate;
 
+public class Employee {
+	private String name;
+	private LocalDate HireDate;
+	private Double Salary;
 
-	import java.time.LocalDate;
+	private Employee() {
+		this.name = name;
+		this.HireDate = HireDate;
+		this.Salary = Salary;
 
+	}
 
-	public class Employee {
-	private  String[] name= {"joy","roy"};
-  private	 LocalDate[] HireDate ;
-	private Double[] Salary= {(double) 100000,(double) 20000};
-	 
-	   private Employee() {
-		   this.name=name;
-		   this.HireDate=HireDate;
-		   this.Salary=Salary;
-	    	
-	    }
-	 public static Employee getemployee(String[] name,LocalDate[] HireDate,Double[] Salary) {
+//	public String toString() {
+//		return "name " + " HireDate" + "Salary";
+//
+//	}
+
+	public static Employee getemployee(String name, LocalDate HireDate, Double Salary) {
 		return new Employee();
-		
-	
-		}
-	 
-	 public String toString() {
-		return "name "+" HireDate" + "Salary";
-		 
-	 }
-	 
-		public static void main(String ar[]) {
-			Employee emp=new Employee();	
-			
-			
-		}
-	
-			
-		}
-         
+
+	}
+
+	public static void main(String[] args) {
+		Employee employee[] = new Employee[3];
+		employee[0] = getemployee("Nayak", LocalDate.of(2010, 11, 11), 123456.11);
+		employee[1] = getemployee("Jay", LocalDate.of(2021, 03, 7), 12233.50);
+		employee[2] = getemployee("Jayant", LocalDate.of(2021, 03, 26), 124523.89);
+	}
 	
 
-
-	
+}
